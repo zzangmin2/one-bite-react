37e5417aa06659e01159688d6f0a6ccdbda3a1c1
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-07-16 22:39:11 +0900

    20230716
    counter 만들기

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,34 +1,15 @@
 // import './App.css';
-import MyHeader from './MyHeader';
+import MyHeader from "./MyHeader";
+import Counter from "./Counter";
 
 function App() {
-    let name="asdfasdfasdf";
-
-    const style={
-        App:{
-            backgroundColor: 'black',
-        },
-        h2:{
-            color: "red",
-        },
-        bold_text: {
-            color: "green",
-        },
-    };
-
-    const number = 5;
-
-
   return (
     // <div className="App">
-      <div style={style.App}>
-        <MyHeader />
-          <h2 style={style.h2}>안녕{name}</h2>
-        <b id="bold_text" style={style.bold_text}>
-            {number}는: {number % 2 === 0? "짝수" : "홀수"}
-        </b>
+    <div>
+      <MyHeader />
+      <Counter />
     </div>
-);
+  );
 }
 
 export default App;
--- reactexam1/src/Counter.js
+++ reactexam1/src/Counter.js
@@ -1,9 +1,31 @@
+import React, {useState} from "react";
+
 const Counter = () =>{
+
+    // 0에서 출발
+    // 1씩 증가하고
+    // 1씩 감소하는
+    //count 상태
+
+    // 0번째 인덱스 -> 상태의 값
+    // 1번째 인덱스 -> 상태변화 함수
+    //useState의 인자 -> 0번째 인덱스의 초기 값
+
+    const [count, setCount] = useState(0);
+
+    const onIncerease = () =>{
+        setCount(count + 1);
+    };
+
+    const onDecrease = () => {
+        setCount(count -1);
+    }
+
     return (
         <div>
-            <h2>0</h2>
-            <button>+</button>
-            <button>-</button>
+            <h2>{count}</h2>
+            <button onClick ={onIncerease}>+</button>
+            <button onClick={onDecrease}>-</button>
         </div>
     )
 }
