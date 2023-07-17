c2a0934448e846916c8d760ac56e0da5b0702775
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-07-17 17:37:11 +0900

    20230717
    state 변화시 리렌더됨.
    useState는 변수명 이름이 다를 경우 여러개 사용 가능.

--- reactexam1/src/Counter.js
+++ reactexam1/src/Counter.js
@@ -11,8 +11,11 @@ const Counter = () =>{
     // 1번째 인덱스 -> 상태변화 함수
     //useState의 인자 -> 0번째 인덱스의 초기 값
 
-    const [count, setCount] = useState(0);
+    console.log("counter 호출");
+    // 자신이 가진 state가 변화하면 리렌더된다. 즉 함수가 다시 호출된다.
 
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
     const onIncerease = () =>{
         setCount(count + 1);
     };
@@ -21,11 +24,23 @@ const Counter = () =>{
         setCount(count -1);
     }
 
+    const onIncerease2 = () =>{
+        setCount2(count2 + 1);
+    };
+
+    const onDecrease2 = () => {
+        setCount2(count2 -1);
+    }
+
     return (
         <div>
             <h2>{count}</h2>
             <button onClick ={onIncerease}>+</button>
             <button onClick={onDecrease}>-</button>
+
+            <h2>{count2}</h2>
+            <button onClick ={onIncerease2}>+</button>
+            <button onClick={onDecrease2}>-</button>
         </div>
     )
 }
