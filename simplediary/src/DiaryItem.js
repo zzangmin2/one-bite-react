3d58a1ebe1e01bbad00e6a1d59ceecc164c815c3
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-08-17 16:58:25 +0900

    230817
    리스트 데이터 삭제

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,4 +1,4 @@
-const DiaryItem = ({ author, content, create_date, emotion, id }) => {
+const DiaryItem = ({ onDelete, author, content, create_date, emotion, id }) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -9,6 +9,16 @@ const DiaryItem = ({ author, content, create_date, emotion, id }) => {
         <span className="date">{new Date(create_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          console.log(id);
+          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
