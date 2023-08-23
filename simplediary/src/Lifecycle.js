dc16889f302fa3fcfb4eb16144ecf5e38497720d
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-08-23 13:52:43 +0900

    20230823
    toggle기능
    unmount됐을 떄 useEffect 사용하는 법

--- simplediary/src/Lifecycle.js
+++ simplediary/src/Lifecycle.js
@@ -1,38 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
+const UnmountTest = () => {
   useEffect(() => {
+    //moun시점에 실행
     console.log("mount!");
+
+    //unmount시점에 실행
+    return () => {
+      console.log("Unmount!");
+    };
   }, []);
 
-  useEffect(() => {
-    console.log("Update!");
-  });
-
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습ㄴ디ㅏ. 따라서 1로 초기화합니다.");
-      setCount(1);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    console.log(`text is update : ${text}`);
-  }, [text]);
+  return <div>Unmount Testing Component</div>;
+};
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
