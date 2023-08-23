4a3dc002672f0fca4003a5f037685c9d2f53efef
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-08-23 23:49:23 +0900

    20230823
    API 호출해서 사용하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -2,12 +2,33 @@ import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const create_date = new Date().getTime();
@@ -37,7 +58,6 @@ function App() {
   };
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
