5ce8a7f2e92199c5093be1f6b94a457200d98b26
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-08-26 21:50:07 +0900

    20230826
    React.Memo를 사용해서 컴포넌트를 메모화해서 재사용

--- simplediary/src/OptimizeTest.js
+++ simplediary/src/OptimizeTest.js
@@ -1,34 +1,57 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`update :: Text: ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`update :: Count: ${count}`);
+    console.log(`Counter A Update - count : ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = ({ obj }) => {
+  useEffect(() => {
+    console.log(`Counter B Update - count: ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  // return true // 이전 프롭스 현재 프롭스가 같다 -> 리렌더링을 일으키지 않게 됨
+  // return false // 이전과 현재가 다르다 -> 리렌더링을 일으키라
+
+  //   if (prevProps.obj.count === nextProps.obj.count) {
+  //     return true;
+  //   }
+  //   return false;
+
+  return prevProps.obj.count === nextProps.obj.count;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-
+  const [obj, setObj] = useState({
+    count: 1,
+  });
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A Button</button>
       </div>
       <div>
-        <h2>text</h2>
-        <TextView text={text} />
-        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button
+          onClick={() => {
+            setObj({
+              count: obj.count,
+            });
+          }}
+        >
+          B Button
+        </button>
       </div>
     </div>
   );
