c2524161f233deb0d8821d09eee812c6265e683b
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2023-09-03 10:04:01 +0900

    20230903
    이미지 에셋 세팅

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouteTest from "./components/RouteTest";
-
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
@@ -13,13 +11,19 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
         <a href={"/new"}>New로 이동</a>
       </div>
     </BrowserRouter>
